7e926c2c5a6c4a82e596b11cb0e164d0d08a537e
gabrielzapata19 <zapata5516763@gmail.com>
2020-04-09 23:42:02 -0500

    Adding deployment scripts

---
+++ build/precache-manifest.7815aeb07eaa5ced23f82c766092a3f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "750330949c01e7ce9829e779118ef024",
+    "url": "/github.io/StaticReact1/index.html"
+  },
+  {
+    "revision": "fcb1378cfb42cd09f104",
+    "url": "/github.io/StaticReact1/static/css/main.b2bd3dab.chunk.css"
+  },
+  {
+    "revision": "57e6e599e2bbdcc22226",
+    "url": "/github.io/StaticReact1/static/js/2.d68a832c.chunk.js"
+  },
+  {
+    "revision": "fcb1378cfb42cd09f104",
+    "url": "/github.io/StaticReact1/static/js/main.ff8710dd.chunk.js"
+  },
+  {
+    "revision": "7d2080bd80a69abc87fb",
+    "url": "/github.io/StaticReact1/static/js/runtime-main.e28bd0e1.js"
+  }
+]);
